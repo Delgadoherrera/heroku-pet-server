efd55cec73370c8e42776b21238a2c792b6514e8
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-13 21:25:45 -0300

    revertir cambio config sequelize

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -12,9 +12,7 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable])[env];
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config, {
-    dialect: process.env.DB_DIALECT
-  })
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
