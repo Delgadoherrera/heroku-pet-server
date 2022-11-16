18366977c642aad627b11f6042d593f3dc7020e6
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-16 15:47:37 -0300

    register

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -123,7 +123,7 @@ router.post("/user/register", async (req, res) => {
                     password: bcryptjs.hashSync(req.body.formData.password, salt),
                     fotoPerfil: req.body.file.base64Data
                 });
-                res.status(200)
+               return res.status(200).send()
             }
             else {
                 Humano.create({
@@ -134,7 +134,7 @@ router.post("/user/register", async (req, res) => {
                     password: bcryptjs.hashSync(req.body.formData.password, salt),
                     fotoPerfil: 'userWithNoAvatar'
                 });
-                return res.status(200)
+                return res.status(200).send()
             }
 
         }
