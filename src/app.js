c7d96809295897e15ee37d51d955e1e58c998538
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-18 19:34:44 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -32,14 +32,14 @@ app.use(
     origin: "*"
   })
 );
-app.use(function (req, res, next) {
+/* app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-});
+}); */
 
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "../public")));
