2148b46e9a5fe55be8e64d94832d1eb0be007adb
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-18 18:55:05 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -27,11 +27,11 @@ const userApi = require("./api/userApi");
 const mascotaApi = require("./api/mascotaApi");
 const mensajesApi = require("./api/mensajesApi");
 
-/* app.use(
+app.use(
   cors({
     origin: "https://missingpets.art",
   })
-); */
+);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://missingpets.art"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
