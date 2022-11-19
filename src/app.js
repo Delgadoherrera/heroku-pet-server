069e0a78e8316cc8a37db90254b055ba4522fd9b
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-19 00:48:59 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -17,10 +17,14 @@ const userApi = require("./api/userApi");
 const mascotaApi = require("./api/mascotaApi");
 const mensajesApi = require("./api/mensajesApi");
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "../public")));
