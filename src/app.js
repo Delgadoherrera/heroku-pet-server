91ea3a36136d1155e3dd4b218ee614580947d027
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-19 00:26:54 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -3,16 +3,6 @@ const path = require("path");
 const cors = require("cors");
 const app = express();
 
-var bodyParser = require("body-parser");
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(
-  bodyParser.urlencoded({
-    limit: "50mb",
-    extended: true,
-    parameterLimit: 50000,
-  })
-);
-
 const db = require("./database/models");
 const Mensaje = db.Mensaje;
 const server = require("http").createServer(app);
@@ -27,29 +17,15 @@ const userApi = require("./api/userApi");
 const mascotaApi = require("./api/mascotaApi");
 const mensajesApi = require("./api/mensajesApi");
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
+var bodyParser = require("body-parser");
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 app.use(cors({ origin: "*", credentials: false }));
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "../public")));
@@ -62,10 +38,10 @@ app.use(
   })
 );
 app.use(express.json({ limit: "10mb", extended: true }));
-app.use(
+/* app.use(
   express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 })
-);
-app.use(express.urlencoded({ extended: false }));
+); */
+/* app.use(express.urlencoded({ extended: false })); */
 /* app.use(cors());
  */
 server.listen(4000);
