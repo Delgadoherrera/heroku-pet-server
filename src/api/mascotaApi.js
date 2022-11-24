482ec40e40eda8d13372cfd5429bbe6d4150e06d
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-24 20:55:58 -0300

    prueba andy

--- src/api/mascotaApi.js
+++ src/api/mascotaApi.js
@@ -260,9 +260,9 @@ router.post("/mascotas/editarMascota/:id", async (req, res) => {
 
 module.exports = router;
 
-router.post("/morfando", async (req, res) => {
-console.log('req.body',req.body)
-console.log('req', req)
+router.get("/morfando", async (req, res) => {
+  console.log('req.body', req.body)
+  console.log('req', req)
 
   res.status(200).send("success");
 });
