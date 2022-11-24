33c53a79cb66f85a77a71e78dde1caddc3b27d78
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-24 14:57:41 -0300

    jwt

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,13 +5,13 @@ const config = process.env;
 const verifyToken = (req, res, next) => {
   console.log(req.headers)
   const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+    req.body.token || req.query.token || req.headers['x-access-token'];
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    const decoded = jwt.verify(token, "algoSecreto");
     req.user = decoded;
   } catch (err) {
     return res.status(401).send("Invalid Token");
