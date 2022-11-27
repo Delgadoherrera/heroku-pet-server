9a982ed086c10731293674f951d63ddac5138db2
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-27 11:53:52 -0300

    auth0

--- src/api/userApi copy.js
+++ src/api/userApi copy.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const multer = require('multer');
-const cors = require('cors');
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
 const router = express.Router();
-const path = require('path')
-const db = require("../database/models")
+const path = require("path");
+const db = require("../database/models");
 const Humano = db.Humano;
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const Sequelize = require('sequelize');
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 /*  const sequelize = new Sequelize('missingpet', 'root', '', {
     host: 'localhost',
@@ -22,148 +22,143 @@ const Op = Sequelize.Op;
 }) 
  */
 
-
-
 router.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../../public/img/img-usuarios"));
-    },
-    filename: (req, file, cb) => {
-        /*    console.log(file); */
-        const newFilename = "file" + Date.now() + path.extname(file.originalname);
-        cb(null, newFilename);
-        req.session.newFileName = newFilename
-    }
-
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../public/img/img-usuarios"));
+  },
+  filename: (req, file, cb) => {
+    /*    console.log(file); */
+    const newFilename = "file" + Date.now() + path.extname(file.originalname);
+    cb(null, newFilename);
+    req.session.newFileName = newFilename;
+  },
 });
-const upload = multer({ storage })
+const upload = multer({ storage });
 
 router.post("/user/login", async (req, res) => {
-    console.log('req.body', req.body)
+  console.log("req.body", req.body);
 
-    let emailVerify = req.body.email
-    let password = req.body.password
-    await Humano.findOne({
-        where: {
+  let emailVerify = req.body.email;
+  let password = req.body.password;
+ /*  await Humano.findOne({
+    where: {
+      email: emailVerify,
+    },
+  }).then(
+    await function (usuario) {
+      if (usuario) {
+        const token = jwt.sign(
+          {
+            email: usuario.email,
+            id: usuario.id,
+          },
+          "algoScreto",
+          { expiresIn: "360m" }
+        );
+        let dbPassword = usuario.password;
+        let key = bcryptjs.compareSync(password, dbPassword);
+        Humano.findOne({
+          where: {
             email: emailVerify,
-        }
-    })
-        .then(await function (usuario) {
-            if (usuario) {
-                const token = jwt.sign({
-                    email: usuario.email,
-                    id: usuario.id
-                }, "algoScreto", { expiresIn: '360m' });
-                let dbPassword = usuario.password;
-                let key = bcryptjs.compareSync(password, dbPassword);
-                Humano.findOne({
-                    where: {
-                        email: emailVerify,
-                        password: key
-                    }
-                }).then(function () {
-                    if (emailVerify === usuario.email && key == true) {
-                        let dataUser = {
-                            nombre: usuario.nombre,
-                            apellido: usuario.apellido,
-                            email: usuario.email,
-                            id: usuario.idHumano,
-                            fotoPerfil: usuario.fotoPerfil
-                        }
-                        return res.status(200).json({
-                            token: token,
-                            dataUser: dataUser
-                        })
-                    }
-                    else if (emailVerify === usuario.email && key == false) {
-                        // contraseña incaasdorrecta == 1
-                        return res.status(200).json('invalid password')
-                    }
-
-                }).catch((error) => {
-                    /* console.log('eraaaaror catch' + error) */
-                })
-
+            password: key,
+          },
+        })
+          .then(function () {
+            if (emailVerify === usuario.email && key == true) {
+              let dataUser = {
+                nombre: usuario.nombre,
+                apellido: usuario.apellido,
+                email: usuario.email,
+                id: usuario.idHumano,
+                fotoPerfil: usuario.fotoPerfil,
+              };
+              return res.status(200).json({
+                token: token,
+                dataUser: dataUser,
+              });
+            } else if (emailVerify === usuario.email && key == false) {
+              return res.status(200).json("invalid password");
             }
-            //El mail no se encuenatra ==3
-            else {
-                return res.status(200).json('No se encuentra el email')
-            }
-        });
+          })
+          .catch((error) => {});
+      } else {
+        return res.status(200).json("No se encuentra el email");
+      }
+    }
+  ); */
+});
 
-}
-)
-const salt = 10
+
+const salt = 10;
 router.post("/user/register", async (req, res) => {
-    let data = (req.body.formData)
-    console.log(req.body.file.base64Data)
-    await Humano.findAll({
-        where: {
+  let data = req.body.formData;
+  console.log(req.body.file.base64Data);
+  await Humano.findAll({
+    where: {
+      email: data.email,
+    },
+  })
+    .then(function (humano) {
+      console.log(humano);
+      if (humano.length > 0) {
+        return res.status(200).send("Porfavor ingresa otro email");
+      } else if (humano.length === 0) {
+        console.log("Este email puede utilizarse");
+        if (req.body.file.base64Data !== null) {
+          Humano.create({
+            nombre: data.name,
+            apellido: data.apellido,
+            telefono: data.telefono,
             email: data.email,
+            password: bcryptjs.hashSync(req.body.formData.password, salt),
+            fotoPerfil: req.body.file.base64Data,
+          });
+          return res.status(200).send("success");
+        } else {
+          Humano.create({
+            nombre: data.name,
+            apellido: data.apellido,
+            telefono: data.telefono,
+            email: data.email,
+            password: bcryptjs.hashSync(req.body.formData.password, salt),
+            fotoPerfil: "userWithNoAvatar",
+          });
+          return res.status(200).send("success");
         }
-    }).then(function (humano) {
-        console.log(humano)
-        if (humano.length > 0) {
-            return res.status(200).send('Porfavor ingresa otro email')
-        }
-        else if (humano.length === 0) {
-            console.log('Este email puede utilizarse')
-            if (req.body.file.base64Data !== null) {
-                Humano.create({
-                    nombre: data.name,
-                    apellido: data.apellido,
-                    telefono: data.telefono,
-                    email: data.email,
-                    password: bcryptjs.hashSync(req.body.formData.password, salt),
-                    fotoPerfil: req.body.file.base64Data
-                });
-               return res.status(200).send('success')
-            }
-            else {
-                Humano.create({
-                    nombre: data.name,
-                    apellido: data.apellido,
-                    telefono: data.telefono,
-                    email: data.email,
-                    password: bcryptjs.hashSync(req.body.formData.password, salt),
-                    fotoPerfil: 'userWithNoAvatar'
-                });
-                return res.status(200).send('success')
-            }
-
-        }
-    }).catch((error) => {
-        console.log('error catch' + error)
+      }
     })
-})
-
+    .catch((error) => {
+      console.log("error catch" + error);
+    });
+});
 
 router.get("/user/userDetail/:id", async (req, res) => {
-    console.log('REQODY', req.params.id)
-    Humano.findAll({
-        where: {
-            idHumano: req.params.id,
-        }
-    }).then(function (humano) {
-        if (humano) {
-            return res.status(200).send({ data: humano })
-        }
-        else if (!humano) {
-            console.log('No se han encontrado mascotas perdidas por tu zona')
-            return res.status(400)
-        }
-    }).catch((error) => {
-        console.log('error catch' + error)
+  console.log("REQODY", req.params.id);
+  Humano.findAll({
+    where: {
+      idHumano: req.params.id,
+    },
+  })
+    .then(function (humano) {
+      if (humano) {
+        return res.status(200).send({ data: humano });
+      } else if (!humano) {
+        console.log("No se han encontrado mascotas perdidas por tu zona");
+        return res.status(400);
+      }
     })
-})
-
-
+    .catch((error) => {
+      console.log("error catch" + error);
+    });
+});
 
 module.exports = router;
-
