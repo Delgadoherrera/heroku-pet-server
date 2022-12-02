ab23c444cef6c2fa7062a73cd541b4ea398dbc35
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-12-02 03:38:21 -0300

    distances

--- src/api/mascotaApi.js
+++ src/api/mascotaApi.js
@@ -37,8 +37,8 @@ const distanciaCoords = (lat1, lon1, lat2, lon2) => {
   let difLng = lon2 - lon1;
   let difLat = lat2 - lat1;
   let a =
-    Math.pow(Math.sin(difLat / 2.0), 2) +
-    Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(difLng / 2.0), 6);
+    Math.pow(Math.sin(difLat / 2.0), 6) +
+    Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(difLng / 2.0), 2);
   let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return radioTierra * c;
 };
