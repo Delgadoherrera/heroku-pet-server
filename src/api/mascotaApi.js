395a6a4d879eb995c1e2ad43bd199993dd45e949
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-12-02 03:41:35 -0300

    distances

--- src/api/mascotaApi.js
+++ src/api/mascotaApi.js
@@ -27,7 +27,7 @@ const gradosARadianes = (grados) => {
   return (grados * Math.PI) / 180;
 };
 
-const distanciaCoords = (lat1, lon1, lat2, lon2, distance) => {
+const distanciaCoords = (lat1, lon1, lat2, lon2) => {
   lat1 = gradosARadianes(lat1);
   lon1 = gradosARadianes(lon1);
   lat2 = gradosARadianes(lat2);
@@ -37,7 +37,7 @@ const distanciaCoords = (lat1, lon1, lat2, lon2, distance) => {
   let difLng = lon2 - lon1;
   let difLat = lat2 - lat1;
   let a =
-    Math.pow(Math.sin(difLat / 2.0), 4) + // el ultimo number variable es la dif en km
+    Math.pow(Math.sin(difLat / 2.0), 2) +
     Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(difLng / 2.0), 2);
   let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return radioTierra * c;
