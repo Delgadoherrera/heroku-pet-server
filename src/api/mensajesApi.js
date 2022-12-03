8be8cc233ea7877c41aa469e1225948ca7ad07f4
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-12-03 04:03:35 -0300

    chat

--- src/api/mensajesApi.js
+++ src/api/mensajesApi.js
@@ -1,82 +1,80 @@
-const express = require('express');
-const multer = require('multer');
-const cors = require('cors');
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
 const router = express.Router();
-const path = require('path')
-const db = require("../database/models")
+const path = require("path");
+const db = require("../database/models");
 const Mensaje = db.Mensaje;
-const jwt = require('jsonwebtoken');
-const Sequelize = require('sequelize');
+const jwt = require("jsonwebtoken");
+const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
-
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../../public/img/pets"));
-    },
-    filename: (req, file, cb) => {
-        /*    console.log(file); */
-        const newFilename = "file" + Date.now() + path.extname(file.originalname);
-        cb(null, newFilename);
-        req.session.newFileName = newFilename
-    }
-
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../public/img/pets"));
+  },
+  filename: (req, file, cb) => {
+    /*    console.log(file); */
+    const newFilename = "file" + Date.now() + path.extname(file.originalname);
+    cb(null, newFilename);
+    req.session.newFileName = newFilename;
+  },
 });
-const upload = multer({ storage })
+const upload = multer({ storage });
 
+const sequelize = new Sequelize("missingPets", "root", "nabuco12", {
+  host: "localhost",
+  dialect: "mysql",
 
-const sequelize = new Sequelize('missingPets', 'root', 'nabuco12', {
-    host: 'localhost',
-    dialect: 'mysql',
-
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-})
-
-
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 
 router.post("/mensajes/nuevoMensaje/", async (req, res) => {
-    console.log(req.body)
+  console.log(req.body);
 
-    await Mensaje.create({
-        mensaje: req.body.msg.msg,
-        idEmisor: req.body.emisor,
-        idReceptor: req.body.receptor,
-        fechaMensaje: req.body.date,
-    });
-    res.status(200).send()
-})
+  await Mensaje.create({
+    mensaje: req.body.msg.msg,
+    idEmisor: req.body.emisor,
+    idReceptor: req.body.receptor,
+    fechaMensaje: req.body.date,
+  });
+  res.status(200).send();
+});
 
-const modelQuery = "select a.mensaje, b.nombre, c.nombre from mensajes a left join humanos b on b.idHumano = a.idEmisor left join humanos c on c.idhumano = a.idReceptor where a.idEmisor ="
-const messageByReceptor = "select a.mensaje, b.nombre , c.idHumano from mensajes a left join humanos b on b.idHumano =a.idEmisor left join humanos c on c.idHumano =b.idhumano where a.idReceptor = "
-const reqForQuery = "  and a.idReceptor ="
+const modelQuery =
+  "select a.mensaje, b.nombre, c.nombre from mensajes a left join humanos b on b.idHumano = a.idEmisor left join humanos c on c.idhumano = a.idReceptor where a.idEmisor =";
+const messageByReceptor =
+  "select a.mensaje, b.nombre , c.idHumano from mensajes a left join humanos b on b.idHumano =a.idEmisor left join humanos c on c.idHumano =b.idhumano where a.idReceptor = ";
+const reqForQuery = "  and a.idReceptor =";
 
-const messageByReceptor2 = "select a.mensaje,c.email from mensajes where a.emailReceptor ="
-const reqForQuery2 = "  and a.emailReceptor ="
-
+const messageByReceptor2 =
+  "select a.mensaje,c.email from mensajes where a.emailReceptor =";
+const reqForQuery2 = "  and a.emailReceptor =";
 
 router.get("/mensajes/getAllMyMsg/:id", async (req, res) => {
-    let id = req.params.id
+  let id = req.params.id;
 
-    await Mensaje.findAll({
-        where: {
-             emailReceptor: id, emailEmisor: idEmisor ,
-                
-        /*         { idReceptor: idEmisor, idEmisor: id }  */
+  await Mensaje.findAll({
+    where: {
+      emailReceptor: id,
+      emailEmisor: idEmisor,
 
-        }}).then(mensajes=>{
-            return res.status(200).send(mensajes)
-        })
-    /*     order: [
+      /*         { idReceptor: idEmisor, idEmisor: id }  */
+    },
+  }).then((mensajes) => {
+    return res.status(200).send(mensajes);
+  });
+  /*     order: [
             ['id', 'DESC'],
 
 
         ], */
-    })
+});
 /* 
     sequelize.query(messageByReceptor2 + id).then(function (mensajes) {
 
@@ -92,35 +90,25 @@ router.get("/mensajes/getAllMyMsg/:id", async (req, res) => {
         console.log('error catch' + error)
     })
  */
-})
-
 
 router.get("/mensajes/getMessagesById/:id/:idEmisor", async (req, res) => {
-    let id = req.params.id
-    let idEmisor = req.params.idEmisor
-    console.log(id, idEmisor)
+  let id = req.params.id;
+  let idEmisor = req.params.idEmisor;
+  console.log(id, idEmisor);
 
-    await Mensaje.findAll({
-        where: {
-            [Op.or]: [{ idReceptor: id, idEmisor: idEmisor },{ idReceptor: idEmisor, idEmisor: id } ],
-
-        },
-        order: [
-            ['id', 'DESC'],
-
-
-        ],
-    }).then(await function (mensajes) {
-        return res.status(200).send({ data: mensajes })
-    })
-
-
-})
-
-
-
+  await Mensaje.findAll({
+    where: {
+      [Op.or]: [
+        { idReceptor: id, idEmisor: idEmisor },
+        { idReceptor: idEmisor, idEmisor: id },
+      ],
+    },
+    order: [["id", "DESC"]],
+  }).then(
+    await function (mensajes) {
+      return res.status(200).send({ data: mensajes });
+    }
+  );
+});
 
 module.exports = router;
-
-
-
